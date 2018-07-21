96810496cdb53bd2f44112ef6c72e9cf73c3a3d5
Jesus-Eder-Gonzalez <jesus.eder.gonzalez@gmail.com>
2018-07-20 14:27:31 -1000

    Added \kick functionality.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const newsBroadcaster = net.createServer();
 newsBroadcaster.on('connection', (socket) => {
   let sAddress = socket.address();
   socket.setEncoding('UTF8');
-  if(!socket.id){
+  if (!socket.id) {
     socket.id = Math.floor(Math.random() * 60000);
   }
 
@@ -23,13 +23,13 @@ newsBroadcaster.on('connection', (socket) => {
 
   users.push(socket);
   socket.on('data', function (data) {
-    if(data.toString().includes('ID:')){
+    if (data.toString().includes('ID:')) {
       socket.customId = data.split(' ')[1];
-      socket.customId = `${socket.customId === 'ADMIN' || socket.customId === 'admin'? `${socket.id}`:`${socket.customId}`}`
-      
-      socket.userName = `${socket.customId} [${sAddress.address}:${sAddress.port}]`;
+      socket.customId = `${socket.customId === 'ADMIN' || socket.customId === 'admin' ? `${socket.id}` : `${socket.customId}`}`
+
+      socket.userName = ` [${sAddress.address}:${sAddress.port}] :: ${socket.customId}`;
     } else {
-      screen.write(`SERVER BCSAST FROM ${socket.userName}: ${data}\n`);
+      screen.write(`SERVER BCSAST FROM ${socket.userName} :: ${socket.id}: ${data}\n`);
       users.forEach(user => user.write(`${socket.userName}: "${data}"\n`));
     }
   });
@@ -42,14 +42,46 @@ newsBroadcaster.on('connection', (socket) => {
 });
 
 keyboardInput.on('line', (line) => {
-  screen.write(`[ADMIN]: "${line}"\n`);
-  users.forEach(user => user.write(`[ADMIN]: "${line}"\n`));
+  if (line.includes('\\')) {
+    let adminAction = line.split(' ')[0].split('\\')[1];
+    let adminWho = line.split(' ')[1];
+    adminActions(adminAction, adminWho);
+  } else {
+    screen.write(`[ADMIN]: ${line}\n`);
+    users.forEach(user => user.write(`[ADMIN]: ${line}\n`));
+  }
 });
 
+function adminActions(action, user) {
+  let line = '';
+  if (!action || !user) {
+    return;
+  }
+  switch (action) {
+    case 'kick':
+      let delinquent = findUser(user)[0];
+      let index = users.indexOf(delinquent);
+      line = `KICKED ${delinquent.customId}`;
+      delinquent.end(`Make better choices.\nGood bye\n`);
+      delinquent.destroy();
+      users.splice(index, 1);
+      break;
+    default:
+      line = `nothing.`;
+      break;
+  }
+  screen.write(`[ADMIN]: "${line}"\n`);
+  users.forEach(user => user.write(`[ADMIN]: ${line}\n`));
+}
+
+function findUser(idStr) {
+  return users.filter(user => user.customId.includes(idStr));
+
+}
 
 process.on('SIGINT', () => {
   users.forEach(user => {
-    user.end(`SERVER DISCONNECTED\n`)
+    user.end(`SERVER DISCONNECTED\n`);
     user.destroy();
   });
   newsBroadcaster.close();
